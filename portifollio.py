2a8f47d345b68e806e189b235558fbf1559c0881
Adriel Alexander <adriel.alexs123@gmail.com>
2023-09-27 00:30:43 -0300

    adicionando estilização e refatorando o código

--- portifollio.py
+++ portifollio.py
@@ -1,5 +1,5 @@
 import streamlit as st
- 
+from styles import estilo
  
 
 st.set_page_config(
@@ -8,72 +8,85 @@ st.set_page_config(
         layout="wide",
         initial_sidebar_state="expanded",
 )
+st.markdown(estilo, unsafe_allow_html=True)
 # Definindo o título do aplicativo
-st.title("Adriel Alexander")
-
-# Adicionando a imagem de perfil com estilo
-st.markdown(
-    f'<img src="https://i.imgur.com/Ux9sLQp.jpg" alt="Foto de Perfil" width="200" height="200" style="border-radius: 50%;">',
+st.write(
+    """
+    <div class="container" style="text-align: center; margin-top: -4px;">
+        <h1 class='titulo-personalizado' style='color: white; background-color: #01392B;'>Adriel Alexander</h1>
+        <div class="profile-img">
+            <img src="https://i.imgur.com/Ux9sLQp.jpg" alt="Foto de Perfil" width="200" height="200" style="border-radius: 50%; max-width: 200px; border: 8px solid #01392B; box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.3); margin: 0 auto; margin-top: 7px;">
+        </div>
+    </div>
+    """,
     unsafe_allow_html=True
 )
-
 # Sidebar com links para as páginas
 pagina_selecionada = st.sidebar.radio("Navegue", ["\U0001F464 Sobre Mim", "\U0001F4AC Soft Skills", "\U0001F4A1 Hard Skills", "\U0001F4BB Projetos", "\U0001F4DA Experiência Acadêmica", "\U0001F4E7 Contato"])
 
 
 # Seção 'Sobre Mim'
 if pagina_selecionada == "\U0001F464 Sobre Mim":
-    st.header("Sobre Mim")
-    markdown_text = """
-    Olá! \U0001F44B Sou um entusiasta de desenvolvimento de software, recentemente graduado no bootcamp de BackEnd Java da DIO. 
-    Tenho uma paixão ardente por programação orientada a objetos, especialmente na linguagem Java \U00002615. 
+    conteudo_sobre_mim = """
+<div class="secao-sobre-mim">
+    <h2 class="titulo-secao">Sobre Mim</h2>
+    <p class="paragrafo-introducao">Olá! &#128075; Sou um entusiasta de desenvolvimento de software, recentemente graduado no bootcamp de BackEnd Java da DIO.</p>
+    <p class="paragrafo-introducao">Tenho uma paixão ardente por programação orientada a objetos, especialmente na linguagem Java &#9749;.</p>
+    <p class="paragrafo-introducao">Além disso, sou estudante de Análise e Desenvolvimento de Sistemas na Faculdade XP, buscando constantemente aprimorar minhas habilidades e conhecimentos.</p>
+    <div class="habilidades">
+        <h3 class="subtitulo">Minhas Habilidades:</h3>
+        <ul class="lista-habilidades">
+            <li class="item-habilidade">Desenvolvimento de Aplicativos Java</li>
+            <li class="item-habilidade">Conhecimentos em MySQL (MariaDB) e NoSQL (MongoDB)</li>
+        </ul>
+    </div>
+    <p class="paragrafo-introducao">Nas horas vagas, sou um grande fã de video games, especialmente League of Legends, onde busco melhorar minhas habilidades em partidas ranqueadas.</p>
+    <p class="paragrafo-introducao">Também sou um orgulhoso dono de um gato de estimação, que é uma fonte constante de alegria e inspiração. &#128049;</p>
+    <p class="paragrafo-introducao">Estou ansioso para contribuir com ideias inovadoras, participar de equipes colaborativas e comunicativas e continuar minha jornada no mundo do desenvolvimento de software.</p>
+    <p class="paragrafo-introducao">Conecte-se comigo no <a class="link-externo" href="https://www.linkedin.com/in/Adriel-alexs/" target="_blank">LinkedIn</a> para oportunidades de aprendizado e colaboração!</p>
+    <p class="paragrafo-introducao">Confira meus trabalhos acadêmicos no <a class="link-externo" href="https://github.com/AdrielProg" target="_blank">GitHub</a> &#128187;</p>
+</div>
+"""
 
-    Além disso, sou estudante de Análise e Desenvolvimento de Sistemas na Faculdade XP, 
-    buscando constantemente aprimorar minhas habilidades e conhecimentos.
-
-    \U0001F4A1 Minhas Habilidades:
-    - Desenvolvimento de Aplicativos Java
-    - Conhecimentos em MySQL (MariaDB) e NoSQL (MongoDB)
-
-    \U0001F3AE Nas horas vagas, sou um grande fã de video games, especialmente League of Legends, 
-    onde busco melhorar minhas habilidades em partidas ranqueadas.
-
-    \U0001F431 Também sou um orgulhoso dono de um gato de estimação, que é uma fonte constante de alegria e inspiração.
-
-    \U00002B50 Estou ansioso para contribuir com ideias inovadoras, participar de equipes colaborativas e comunicativas e 
-    continuar minha jornada no mundo do desenvolvimento de software.
-
-    Conecte-se comigo no [LinkedIn](https://www.linkedin.com/in/Adriel-alexs/) para oportunidades de aprendizado e colaboração!  
-    
-    Confira meus trabalho acadêmicos no [GitHub](https://github.com/AdrielProg). \U0001F4BB
-    """
-    
-    st.markdown(markdown_text, unsafe_allow_html=True)
+    st.markdown(conteudo_sobre_mim, unsafe_allow_html=True)
 
 # Seção 'Habilidades'
 elif pagina_selecionada == "\U0001F4AC Soft Skills":
     st.header("Habilidades Interpessoais")
-    st.markdown("# Proposta do Plano de Desenvolvimento Individual (PDI)")
+    titulo_html = "<h1 style='color: white;'>Proposta do Plano de Desenvolvimento Individual (PDI)</h1>"
+    st.markdown(titulo_html, unsafe_allow_html=True)
+    conteudo_habilidades_interpessoais = """
+<p>O Plano de Desenvolvimento Individual (PDI) é uma ferramenta valiosa para o meu crescimento pessoal e profissional. Através deste plano, busco aprimorar minhas habilidades, adquirir novos conhecimentos e alcançar metas significativas. <a class="link-externo" href="https://online.igti.com.br/eportfolios/2585?verifier=W6qU2RuJtOLvvZQzQLFFhskQ2tOzOdxP3PIn9kZv" target="_blank">Confira meu PDI</a></p>
 
-    st.write("O Plano de Desenvolvimento Individual (PDI) é uma ferramenta valiosa para o meu crescimento pessoal e profissional. Através deste plano, busco aprimorar minhas habilidades, adquirir novos conhecimentos e alcançar metas significativas. [Confira meu PDI](https://online.igti.com.br/eportfolios/2585?verifier=W6qU2RuJtOLvvZQzQLFFhskQ2tOzOdxP3PIn9kZv)")
-    
+<div class="lista-container">
+<h2 class="subtitulo">Objetivos do PDI</h2>
+    <ul class="lista-objetivos">
+        <li><strong>Desenvolvimento Profissional</strong>: Através do PDI, pretendo aperfeiçoar minhas habilidades técnicas e competências profissionais, tornando-me um membro mais eficaz e valioso para a equipe.</li>
+        <li><strong>Aprendizado Contínuo</strong>: Estabeleci metas de aprendizado contínuo, incluindo a exploração de novas tecnologias, aprofundamento em conceitos-chave e a participação em cursos relevantes.</li>
+        <li><strong>Crescimento Pessoal</strong>: Além do desenvolvimento profissional, o PDI também se concentra no meu crescimento pessoal, promovendo habilidades interpessoais, resiliência emocional e equilíbrio entre trabalho e vida pessoal.</li>
+        <li><strong>Contribuição para a Comunidade</strong>: Tenho o objetivo de aplicar o que aprendo não apenas em benefício próprio, mas também para contribuir positivamente para a comunidade e para as equipes com as quais colaboro.</li>
+    </ul>
+</div>
 
-    st.markdown("## Objetivos do PDI")
-    st.write("- **Desenvolvimento Profissional**: Através do PDI, pretendo aperfeiçoar minhas habilidades técnicas e competências profissionais, tornando-me um membro mais eficaz e valioso para a equipe.")
-    st.write("- **Aprendizado Contínuo**: Estabeleci metas de aprendizado contínuo, incluindo a exploração de novas tecnologias, aprofundamento em conceitos-chave e a participação em cursos relevantes.")
-    st.write("- **Crescimento Pessoal**: Além do desenvolvimento profissional, o PDI também se concentra no meu crescimento pessoal, promovendo habilidades interpessoais, resiliência emocional e equilíbrio entre trabalho e vida pessoal.")
-    st.write("- **Contribuição para a Comunidade**: Tenho o objetivo de aplicar o que aprendo não apenas em benefício próprio, mas também para contribuir positivamente para a comunidade e para as equipes com as quais colaboro.")
+<div class="lista-container">
+<h2 class="subtitulo">Metodologia do PDI</h2>
+    <ul class="lista-metodologia">
+        <li><strong>70%</strong>: A maior parte do meu desenvolvimento virá da experiência prática e do trabalho do dia-a-dia.</li>
+        <li><strong>20%</strong>: Também valorizo a aprendizagem por meio da interação com colegas e mentores, trocando experiências e conhecimentos.</li>
+        <li><strong>10%</strong>: Complementarei meu PDI com aprendizado formal, incluindo cursos, leituras e workshops.</li>
+    </ul>
+</div>
+<div class="lista-container">
+<h2 class="subtitulo">Compromisso com o PDI</h2>
+<p>Este PDI reflete meu compromisso com o crescimento e o aprimoramento contínuos. Acredito que investir em meu próprio desenvolvimento é fundamental para alcançar meus objetivos profissionais e pessoais.</p>
+<p>Ao longo deste processo, estou ansioso para aprender, evoluir e contribuir de maneira significativa para as equipes e comunidades das quais faço parte.</p>
+<p>Juntos, podemos construir um futuro brilhante e cheio de oportunidades.</p>
+</div>
+"""
 
-    st.markdown("## Metodologia do PDI")
-    st.write("O PDI será conduzido de acordo com o modelo 70/20/10, que enfatiza a importância de diferentes formas de aprendizado:")
-    st.write("- **70%**: A maior parte do meu desenvolvimento virá da experiência prática e do trabalho do dia-a-dia.")
-    st.write("- **20%**: Também valorizo a aprendizagem por meio da interação com colegas e mentores, trocando experiências e conhecimentos.")
-    st.write("- **10%**: Complementarei meu PDI com aprendizado formal, incluindo cursos, leituras e workshops.")
+    st.markdown(conteudo_habilidades_interpessoais, unsafe_allow_html=True)
 
-    st.markdown("## Compromisso com o PDI")
-    st.write("Este PDI reflete meu compromisso com o crescimento e o aprimoramento contínuos. Acredito que investir em meu próprio desenvolvimento é fundamental para alcançar meus objetivos profissionais e pessoais.")
-    st.write("Ao longo deste processo, estou ansioso para aprender, evoluir e contribuir de maneira significativa para as equipes e comunidades das quais faço parte.")
-    st.write("Juntos, podemos construir um futuro brilhante e cheio de oportunidades.")
+
    
 
 # Seção 'Projetos'
@@ -81,21 +94,27 @@ elif pagina_selecionada == "\U0001F4BB Projetos":
     st.header("Projetos")
     st.write("Aqui estão alguns dos projetos em que trabalhei:")
 
-    # Projeto 1
-    st.write("## FinanceiraMente")
-    st.write("Bem-vindo ao projeto FinanceiraMente! Este é um projeto aplicado da Faculdade XPE desenvolvido pelo Squad 1. O objetivo principal deste projeto é promover a educação financeira de forma simples e interativa, tornando os conceitos financeiros básicos mais acessíveis ao público em geral. O FinanceiraMente é uma página web interativa que oferece uma variedade de recursos para ensinar educação financeira de maneira envolvente e intuitiva, incluindo um jogo de perguntas e respostas, calculadoras financeiras e conteúdo educativo.")
-    st.write("[Clique aqui para acessar o FinanceiraMente](https://projetoaplicadoxpe-financeiramente.streamlit.app)")
+    conteudo_projetos = """
+    <div class="projeto">
+        <h3 class="titulo-projeto">FinanceiraMente</h3>
+        <p>Bem-vindo ao projeto FinanceiraMente! Este é um projeto aplicado da Faculdade XPE desenvolvido pelo Squad 1. O objetivo principal deste projeto é promover a educação financeira de forma simples e interativa, tornando os conceitos financeiros básicos mais acessíveis ao público em geral. O FinanceiraMente é uma página web interativa que oferece uma variedade de recursos para ensinar educação financeira de maneira envolvente e intuitiva, incluindo um jogo de perguntas e respostas, calculadoras financeiras e conteúdo educativo.</p>
+        <a class="link-externo" href="https://projetoaplicadoxpe-financeiramente.streamlit.app" target="_blank">Clique aqui para acessar o FinanceiraMente</a>
+    </div>
+    <div class="projeto">
+        <h3 class="titulo-projeto">X-men Project</h3>
+        <p>O projeto 'X-men Project' é uma aplicação interativa desenvolvida em JavaScript, HTML e CSS que permite aos usuários explorar e aprender mais sobre os famosos personagens da série de quadrinhos X-Men. Este projeto oferece uma experiência envolvente, onde os usuários podem selecionar diferentes personagens e obter informações detalhadas sobre eles.</p>
+        <a class="link-externo" href="https://adrielprog.github.io/x-men-project/" target="_blank">Clique aqui para acessar o X-men Project</a>
+    </div>
+    <div class="projeto">
+        <h3 class="titulo-projeto">Simulador de iPhone em Java</h3>
+        <p>Este é um projeto de Simulador de iPhone em Java que visa demonstrar conceitos de programação orientada a objetos.</p>
+        <a class="link-externo" href="https://github.com/AdrielProg/Dio-Trilha-Java-Basico-Diagrama-Iphone" target="_blank">Clique aqui para acessar o projeto no GitHub</a>
+    </div>
+</div>
+"""
 
-    st.write("## X-men Project")
-    st.write("O projeto 'X-men Project' é uma aplicação interativa desenvolvida em JavaScript, HTML e CSS que permite aos usuários explorar e aprender mais sobre os famosos personagens da série de quadrinhos X-Men. Este projeto oferece uma experiência envolvente, onde os usuários podem selecionar diferentes personagens e obter informações detalhadas sobre eles.")
-    st.write("[Clique aqui para acessar o X-men Project](https://adrielprog.github.io/x-men-project/)")
-    # Projeto 2
-   
+    st.markdown(conteudo_projetos, unsafe_allow_html=True)
 
-# Título do projeto e link
-    st.write("## Simulador de iPhone em Java")
-    st.write("Este é um projeto de Simulador de iPhone em Java que visa demonstrar conceitos de programação orientada a objetos.")
-    st.write("[Clique aqui para acessar o projeto no GitHub](https://github.com/AdrielProg/Dio-Trilha-Java-Basico-Diagrama-Iphone)")
 
 
 # Seção 'Contato'
@@ -131,32 +150,36 @@ elif pagina_selecionada == "\U0001F4A1 Hard Skills":
 
 # Seção 'Experiência Acadêmica'
 elif pagina_selecionada == "\U0001F4DA Experiência Acadêmica":
-    st.header("Experiência Acadêmica")
-    st.write("### Ensino Superior")
+    experiencia = """
 
+    <h1>Experiência Acadêmica</h1>
 
-    st.write("**XP Educação**")
-    st.write("Análise e Desenvolvimento de Sistemas")
-    st.write("Outubro de 2022 - 2025")
-    st.write("Competências: Programação Orientada a Objetos (POO), python, HTML, CSS")
+    <div class="categoria">
+        <h2>Ensino Superior</h2>
+            <h3>XP Educação</h3>
+            <p>Análise e Desenvolvimento de Sistemas</p>
+            <p>Outubro de 2022 - 2025</p>
+            <p>Competências: Programação Orientada a Objetos (POO), Python, HTML, CSS</p>
+        </div>
+    <div class="categoria">
+        <h2>Ensino Médio</h2>
+            <h3>IFRN</h3>
+            <p>Ensino Médio, Técnico em Eletrotécnica</p>
+            <p>Maio de 2014 - Dezembro de 2018</p>
+            <p>Competências: Inglês</p>
+        </div>
+    <div class="categoria">
+        <h2>Demais Cursos e Bootcamps</h2>
+            <h3>Udemy Alumni</h3>
+            <p>Java Completo 2023 Programação Orientada a Objetos + Projetos</p>
+            <p>Nota: Em andamento</p>
+            <p>Curso completo de Java e OO, UML, JDBC, JavaFX, Spring Boot, JPA, Hibernate, MySQL, MongoDB</p>
+            <p>Competências: Java · Programação Orientada a Objetos (POO)</p>  
+    </div>
 
-    
-    st.write("### Ensino Médio")
+"""
 
-
-    st.write("**IFRN**")
-    st.write("Ensino Médio, Técnico em Eletrotécnica")
-    st.write("Maio de 2014 - Dezembro de 2018")
-    st.write("Competências: Inglês")
-
-
-    st.write("### Demais Cursos e Bootcamps")
-   
-    st.write("**Udemy Alumni**")
-    st.write("Java Completo 2023 Programação Orientada a Objetos + Projetos")
-    st.write("Nota: Em andamento")
-    st.write("Curso completo de Java e OO, UML, JDBC, JavaFX, Spring Boot, JPA, Hibernate, MySQL, MongoDB")
-    st.write("Competências: Java · Programação Orientada a Objetos (POO)")
+    st.markdown(experiencia, unsafe_allow_html=True)
 
 
     # Rodapé
---
+++ styles.py
@@ -0,0 +1,153 @@
+estilo = '''
+<style>
+.titulo-personalizado{
+ margin: 20px 0;
+    padding: 20px;
+    border: 2px solid #00A86B;
+    border-radius: 10px;
+    background-color: #003528;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+}
+.container {
+    text-align: center;
+    margin-top: -5px; /* Mover o container 5px para cima */
+}
+/* Estilo para a seção "Sobre Mim" */
+.secao-sobre-mim {
+    margin: 20px 0;
+    padding: 20px;
+    border: 2px solid #00A86B;
+    border-radius: 10px;
+    background-color: #003528;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+/* Título da seção */
+.titulo-secao {
+    font-size: 24px;
+    margin-bottom: 20px;
+}
+/* Estilo para os parágrafos de introdução */
+.paragrafo-introducao {
+    font-size: 18px;
+    line-height: 1.5;
+    margin-bottom: 15px;
+}
+/* Estilo para as habilidades */
+.habilidades {
+    margin-top: 20px;
+}
+.lista-container {
+   margin: 20px 0;
+    padding: 20px;
+    border: 2px solid #00A86B;
+    border-radius: 10px;
+    background-color: #003528;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+/* Título das habilidades */
+.subtitulo {
+    font-size: 20px;
+    margin-bottom: 10px;
+}
+
+/* Lista de habilidades */
+.lista-habilidades {
+    font-size: 16px;
+    list-style-type: square;
+    padding-left: 20px;
+}
+/* Estilo para os links externos */
+.link-externo {
+    color: #00A86B;
+    text-decoration: none;
+    font-weight: bold;
+}
+.link-externo:hover {
+    text-decoration: underline;
+}
+/* Estilo para a seção de Projetos */
+.projeto {
+    margin: 20px 0;
+    padding: 20px;
+    border: 2px solid #00A86B;
+    border-radius: 10px;
+    background-color: #003528;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+/* Título do projeto */
+.titulo-projeto {
+    font-size: 20px;
+    color: #FFFFFF;
+    margin-bottom: 10px;
+}
+/* Descrição do projeto */
+.projeto p {
+    font-size: 16px;
+    color: #FFFFFF;
+    line-height: 1.5;
+}
+/* Estilo de links para projetos */
+.link-externo {
+    color: #00A86B;
+    text-decoration: none;
+    font-weight: bold;
+}
+
+.subtitulo {
+    color: white; /* Define a cor do texto para branco */
+    font-size: 24px; /* Tamanho da fonte do título */
+    margin-top: 0; /* Remove o espaçamento superior padrão do título */
+}
+
+.lista-objetivos,
+.lista-metodologia {
+    list-style-type: disc; /* Define o tipo de marcador da lista */
+    color: white; /* Define a cor do texto das listas para branco */
+    padding-left: 20px; /* Adiciona um recuo à esquerda nas listas */
+}
+
+.lista-objetivos li,
+.lista-metodologia li {
+    margin-bottom: 10px; /* Espaçamento entre os itens da lista */
+}
+h1 {
+    font-size: 28px;
+    color: #333;
+    margin-bottom: 20px;
+}
+
+/* Estilo para cada categoria */
+.categoria {
+    margin-bottom: 20px;
+    background-color: #003528;
+    border: 1px solid #ccc;
+    border-radius: 10px;
+    padding: 20px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    color: #FFFFFF
+}
+
+/* Estilo para o título da categoria */
+.categoria h2 {
+    font-size: 24px;
+    color: #FFFFFF;
+    margin-bottom: 10px;
+}
+
+/* Estilo para o nome da instituição */
+.categoria h3 {
+    font-size: 18px;
+    color: #007bff;
+    margin-bottom: 5px;
+}
+
+/* Estilo para os detalhes da instituição */
+.categoria p {
+    font-size: 16px;
+    color: #FFFFFF;
+    margin: 5px 0;
+}
+</style>
+'''
